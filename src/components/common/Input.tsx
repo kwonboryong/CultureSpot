d0e26580802c5e55c933c8f2f6642c4dd09adec2
kwonboryong <kwonboryong@naver.com>
2025-03-09 13:28:55 +0900

    style: Input css 코드 이동

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -12,17 +12,14 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            'focus-visible:ring-ring flex h-10 w-full bg-bg-light px-4 py-1 text-sm transition-colors placeholder:text-text-disabled focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-            className,
-            'pr-10',
-            'rounded-lg'
+            'focus-visible:ring-ring flex h-10 w-full rounded-lg bg-bg-light px-4 py-1 pr-16 text-sm transition-colors placeholder:text-text-disabled focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm'
           )}
           ref={ref}
           {...props}
         />
 
         {icon && (
-          <div className='absolute right-3 top-1/2 -translate-y-1/2 transform cursor-pointer text-xs'>
+          <div className='absolute text-xs transform -translate-y-1/2 cursor-pointer right-3 top-1/2'>
             {icon}
           </div>
         )}
