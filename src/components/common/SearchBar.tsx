81f7e838e96396137fc7cee26c03a6f27b3b3ee0
kwonboryong <kwonboryong@naver.com>
2025-03-09 13:40:08 +0900

    style: input height 조정 및 돋보기 아이콘 추가

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,6 +1,6 @@
 import { InputHTMLAttributes } from 'react';
 import { cn } from 'src/lib/utils';
-// import { IoSearchOutline } from 'react-icons/io5';
+import { IoIosSearch } from 'react-icons/io';
 
 interface SearchInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
@@ -10,13 +10,13 @@ const SearchBar = ({ ...props }: SearchInputProps) => {
       <input
         type='text'
         className={cn(
-          'flex h-10 w-full rounded-2xl bg-bg-light px-4 py-1 pr-16 text-sm transition-colors placeholder:text-text-disabled focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm'
+          'flex h-9 w-full rounded-3xl bg-bg-light px-4 py-1 pr-16 text-sm transition-colors placeholder:text-text-disabled focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm'
         )}
         {...props}
       />
 
       <div className='absolute text-xs transform -translate-y-1/2 cursor-pointer right-3 top-1/2'>
-        돋보기
+        <IoIosSearch className='w-6 h-6 text-text-sub' />
       </div>
     </div>
   );
