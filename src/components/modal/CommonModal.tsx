fd9ae6e0116f98de595fc2af90986da7b0103ea0
kwonboryong <kwonboryong@naver.com>
2025-03-13 20:48:21 +0900

    style: CommonModal 컴포넌트 생성, 스타일 추가, props 추가

---
+++ src/components/modal/CommonModal.tsx
@@ -0,0 +1,37 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './Dialog';
+
+interface CommonModalProps {
+  variant?: 'delete' | 'fix' | 'withdraw';
+  customText?: string;
+}
+
+const textVariants = {
+  delete: '정말 삭제하시겠습니까?',
+  fix: '수정 사항이 저장되지 않았습니다. \n 계속 하시겠습니까?',
+  withdraw: '탈퇴 시 모든 정보가 사라집니다. \n 계속 하시겠습니까?',
+};
+
+const CommonModal = ({ variant, customText }: CommonModalProps) => {
+  const modalText = variant ? textVariants[variant] : customText;
+
+  return (
+    <Dialog>
+      <DialogTrigger>모달 Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle style={{ whiteSpace: 'pre-line' }}>
+            {modalText}
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CommonModal;
