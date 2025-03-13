52a2a2b45eb77cd8172946b37d2cc48ca1cf59e9
kwonboryong <kwonboryong@naver.com>
2025-03-13 19:31:30 +0900

    style: 햄버거 아이콘 및 알림 아이콘 추가, 로고 클릭 시 메인 페이지로 이동

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,22 +1,36 @@
+import Link from 'next/link';
+import Icon from '../Icons/Icon';
 import SearchBar from './SearchBar';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 const Header = () => {
   return (
-    <div className='flex items-center justify-between h-20'>
-      <div className='ml-5 flex w-[25%] gap-4'>
-        <div>햄버거</div>
-        <img src='/assets/Logo.svg' className='h-9 w-60' />
+    <header className='flex items-center justify-between h-20'>
+      <div className='ml-7 flex w-[25%] gap-10'>
+        <button>
+          <RxHamburgerMenu className='w-5 h-5' />
+        </button>
+
+        <Link href={'/'}>
+          <img
+            src='/assets/Logo.svg'
+            className='cursor-pointer h-9 w-60'
+            alt='Logo'
+          />
+        </Link>
       </div>
 
       <div className='w-[25%]'>
         <SearchBar />
       </div>
 
-      <div className='mr-5 flex w-[25%] justify-end gap-4'>
-        <div>알림</div>
-        <div>드롭다운 메뉴</div>
+      <div className='mr-7 flex w-[25%] justify-end gap-4'>
+        <button>
+          <Icon name='NOTIFICATION' size={23} />
+        </button>
+        <div>드롭다운</div>
       </div>
-    </div>
+    </header>
   );
 };
 
