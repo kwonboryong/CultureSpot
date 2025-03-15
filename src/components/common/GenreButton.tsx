e3e8b6c3645e23bf5b7b7dfacfcce801b3f20489
kwonboryong <kwonboryong@naver.com>
2025-03-15 21:26:17 +0900

    fix: 코드리뷰 피드백 반영

--- src/components/common/GenreButton.tsx
+++ src/components/common/GenreButton.tsx
@@ -4,7 +4,7 @@ import Icon from '../Icons/Icon';
 
 interface GenreButtonProps {
   genre: string;
-  mode: 'toggle' | 'removable';
+  mode: 'toggle' | 'removable' | 'black-toggle';
   onRemove?: (genre: string) => void;
 }
 
@@ -13,21 +13,35 @@ const GenreButton = ({ genre, mode, onRemove }: GenreButtonProps) => {
 
   // A 모드: 선택/해제 토글
   const handleClick = () => {
-    if (mode === 'toggle') {
+    if (mode === 'toggle' || mode === 'black-toggle') {
       setSelected((prev) => !prev);
     }
   };
 
+  const modeStyles: Record<string, string> = {
+    removable: 'bg-primary text-white font-semibold hover:bg-primary-hover',
+    toggle: selected
+      ? 'border-none bg-primary text-white font-semibold hover:bg-primary-hover'
+      : 'border border-border bg-white text-text hover:bg-primary-main100',
+    'black-toggle': selected
+      ? 'border-none bg-black text-white font-semibold hover:bg-gray-800'
+      : 'border border-border bg-white text-text hover:bg-gray-300',
+  };
+
   return (
     <button
-      onClick={mode === 'toggle' ? handleClick : undefined}
-      className={`m-1 rounded-10 border border-border px-2.5 py-1.5 text-body2 ${selected ? 'bg-primary text-white hover:bg-primary-hover' : 'bg-white text-text hover:bg-primary-main100'} `}
+      onClick={handleClick}
+      className={`m-1 rounded-10 px-2.5 py-1.5 text-body2 ${modeStyles[mode]}`}
     >
       <div className='flex items-center'>
         {genre}
         {mode === 'removable' && (
           <span onClick={() => onRemove?.(genre)}>
-            <Icon name='CLOSE' size={8} className='ml-2' />
+            <Icon
+              name='CLOSE'
+              size={8}
+              className='ml-2 stroke-white stroke-[3]'
+            />
           </span>
         )}
       </div>
