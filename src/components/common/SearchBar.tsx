6a64c6e92b195198770b92f3b555dd1d1787d6af
HY KIM <chopinoff@gmail.com>
2025-03-17 06:02:18 +0900

    refactor: Header에 Dropdown 추가 및 스타일 수정

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -7,13 +7,12 @@ interface SearchInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 const SearchBar = ({ ...props }: SearchInputProps) => {
   return (
-    <div className='relative w-full rounded-lg'>
+    <div className={cn('relative w-full rounded-lg', props.className)}>
       <input
         type='text'
         className={cn(
           'flex h-9 w-full rounded-3xl bg-bg-light px-4 py-1 pr-16 text-sm transition-colors placeholder:text-text-disabled focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm'
         )}
-        {...props}
       />
 
       <div className='absolute right-3 top-1/2 -translate-y-1/2 transform cursor-pointer text-xs'>
