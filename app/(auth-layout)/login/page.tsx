4a686df607a8dc5dd75d9fe972ce3fafb81c8e1e
kwonboryong <kwonboryong@naver.com>
2025-03-17 15:11:23 +0900

    feat: login 페이지  파일 생성 및 소셜 로그인 버튼 추가

---
+++ app/(auth-layout)/login/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'src/components/Common/Button';
+
+export default function Home() {
+  return (
+    <div className='flex flex-col gap-3'>
+      <Button variant='secondary' children='Google로 로그인' size={'lg'} />
+      <Button variant='kakao' children='카카오로 로그인' size={'lg'} />
+    </div>
+  );
+}
