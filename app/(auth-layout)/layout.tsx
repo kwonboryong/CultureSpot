07dbe4b165b21313bc0be2e4b2402891c2e2fefd
kwonboryong <kwonboryong@naver.com>
2025-03-17 15:10:59 +0900

    feat: auth-layout 파일 생성 및 레이아웃 구조 설정

---
+++ app/(auth-layout)/layout.tsx
@@ -0,0 +1,32 @@
+import '../../styles/globals.css';
+import React from 'react';
+
+export const metadata = {
+  title: '로그인 - My Next.js App',
+  description: '로그인하여 더 많은 서비스를 이용해보세요.',
+};
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='ko'>
+      <body className='flex justify-center'>
+        <main className='mt-[18vh] flex w-full flex-col items-center'>
+          <img src='/assets/Logo.svg' alt='CultureSpot 로고' />
+          <p className='pt-4 caret-transparent'>
+            로그인 후 더 많은 서비스를 사용해보세요
+          </p>
+          <img
+            src='/assets/logo-icon.svg'
+            alt='CultureSpot 로고 아이콘'
+            className='m-10 no-cursor w-44'
+          />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
