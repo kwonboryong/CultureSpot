0edd3324ddfff01ef5b5e961952e59b9bb6871e4
kwonboryong <kwonboryong@naver.com>
2025-03-17 15:34:18 +0900

    feat: framer motion을 이용하여 로고 애니메이션 추가

--- app/(auth-layout)/layout.tsx
+++ app/(auth-layout)/layout.tsx
@@ -1,10 +1,12 @@
+'use client';
 import '../../styles/globals.css';
 import React from 'react';
+import { motion } from 'framer-motion';
 
-export const metadata = {
-  title: '로그인 - My Next.js App',
-  description: '로그인하여 더 많은 서비스를 이용해보세요.',
-};
+// export const metadata = {
+//   title: '로그인 - My Next.js App',
+//   description: '로그인하여 더 많은 서비스를 이용해보세요.',
+// };
 
 export default function AuthLayout({
   children,
@@ -19,10 +21,19 @@ export default function AuthLayout({
           <p className='pt-4 caret-transparent'>
             로그인 후 더 많은 서비스를 사용해보세요
           </p>
-          <img
+          <motion.img
             src='/assets/logo-icon.svg'
             alt='CultureSpot 로고 아이콘'
-            className='m-10 no-cursor w-44'
+            className='no-cursor m-9 w-44'
+            animate={{
+              rotateY: [0, 360],
+            }}
+            transition={{
+              duration: 3,
+              repeat: Infinity,
+              repeatType: 'loop',
+              ease: 'backInOut',
+            }}
           />
           {children}
         </main>
