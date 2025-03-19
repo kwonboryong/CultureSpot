701c31cade25084eea75b500b2983c7c329ade9e
kwonboryong <kwonboryong@naver.com>
2025-03-19 11:12:19 +0900

    fix: SidebarProfile에서 설정 아이콘 클릭 시 프로필 페이지로 경로 수정 및 설정 아이콘 hover 스타일 추가

--- src/components/Common/SidebarProfile.tsx
+++ src/components/Common/SidebarProfile.tsx
@@ -15,18 +15,18 @@ export default function SidebarProfile({ isCollapsed }: SidebarProfileProps) {
         isCollapsed ? 'h-[60px]' : 'h-[180px]'
       )}
     >
-      <Link href={'/my/dashboard'} className='relative'>
+      <Link href={'/my/profile'} className='relative'>
         <Avatar name={'user'} variant='beam' size={isCollapsed ? 30 : 100} />
         <div
           className={cn(
-            'absolute bottom-0 right-0 flex h-[30px] w-[30px] items-center justify-center rounded-20 bg-bg-dark',
+            'group absolute bottom-0 right-0 flex h-[30px] w-[30px] items-center justify-center rounded-20 bg-bg-dark hover:bg-gray-500',
             isCollapsed && 'opacity-0 hover:opacity-50'
           )}
         >
           <Icon
             name='EDIT'
             size={16}
-            className='fill-bg-dark stroke-white stroke-[2px]'
+            className='fill-bg-dark stroke-white stroke-[2px] group-hover:fill-gray-500'
           />
         </div>
       </Link>
