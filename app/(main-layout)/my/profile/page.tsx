cd850c87c0dfa3f9622ec1b7adab44c1e5a95c8b
kwonboryong <kwonboryong@naver.com>
2025-03-19 17:18:40 +0900

    feat: 장르 버튼 추가

--- app/(main-layout)/my/profile/page.tsx
+++ app/(main-layout)/my/profile/page.tsx
@@ -2,28 +2,49 @@
 import { Button } from 'src/components/Common/Button';
 import Input from 'src/components/Common/Input';
 import Icon from 'src/icons/Icon';
+import GenreButton from './../../../../src/components/Common/GenreButton';
+import AvatarProfile from 'src/components/Common/AvatarProfile';
+
+// 상수 처리
+const genres = [
+  { type: '공연', name: '연극' },
+  { type: '공연', name: '뮤지컬' },
+  { type: '공연', name: '콘서트' },
+  { type: '공연', name: '클래식' },
+  { type: '공연', name: '무용' },
+  { type: '공연', name: '전통 공연' },
+  { type: '공연', name: '코미디' },
+  { type: '전시', name: '미술 전시' },
+  { type: '전시', name: '현대 미술' },
+  { type: '전시', name: '과학/역사 전시' },
+];
 
 export default function Profile() {
   return (
-    <div className='flex flex-col items-center justify-center gap-3 p-5 mx-auto'>
+    <div className='mx-auto flex flex-col items-center justify-center gap-3 p-5'>
       <p className='text-h3'>회원 정보 수정</p>
+
+      <AvatarProfile />
 
       <form action='' className='flex flex-col gap-5'>
         <Input type='email' placeholder='기존 이메일 값' />
         <Input
           type='password'
           placeholder='비밀번호'
-          icon={<Icon name='EYE' size={40} className='text-black bg-black' />}
+          icon={<Icon name='EYE' size={40} className='bg-black text-black' />}
         />
         <Input
           type='password'
           placeholder='비밀번호 확인'
-          icon={<Icon name='EYE' size={40} className='text-black bg-black' />}
+          icon={<Icon name='EYE' size={40} className='bg-black text-black' />}
         />
         <Input type='text' icon='중복 확인' placeholder='닉네임' />
 
-        <div className='bg-teal-300'>
+        <div className=''>
           <p>관심 장르 설정</p>
+          {genres.map((genre, i) => (
+            <GenreButton key={i} genre={genre.name} mode={'toggle'} />
+          ))}
         </div>
       </form>
 
