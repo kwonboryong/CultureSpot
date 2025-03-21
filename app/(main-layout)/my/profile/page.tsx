dd34e1ef57991656dc134ed373de2d485b215a45
kwonboryong <kwonboryong@naver.com>
2025-03-21 20:02:12 +0900

    feat: 비밀번호 보이기/숨기기 기능을 위한 아이콘 및 state 추가 - BE 측에서 전달한 장르 중분류에 따른 장르 상수값 수정

--- app/(main-layout)/my/profile/page.tsx
+++ app/(main-layout)/my/profile/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useState } from 'react';
 import { Button } from 'src/components/Common/Button';
 import Input from 'src/components/Common/Input';
 import Icon from 'src/icons/Icon';
@@ -7,48 +8,82 @@ import AvatarProfile from 'src/components/Common/AvatarProfile';
 
 // 상수 처리
 const genres = [
+  { type: '공연', name: '전시' },
   { type: '공연', name: '연극' },
-  { type: '공연', name: '뮤지컬' },
-  { type: '공연', name: '콘서트' },
-  { type: '공연', name: '클래식' },
+  { type: '공연', name: '음악' },
   { type: '공연', name: '무용' },
-  { type: '공연', name: '전통 공연' },
-  { type: '공연', name: '코미디' },
-  { type: '전시', name: '미술 전시' },
-  { type: '전시', name: '현대 미술' },
-  { type: '전시', name: '과학/역사 전시' },
+  { type: '공연', name: '미술' },
+  { type: '공연', name: '건축' },
+  { type: '공연', name: '영상' },
+  { type: '전시', name: '문학' },
+  { type: '전시', name: '문화 정책' },
+  { type: '전시', name: '축제' },
+  { type: '전시', name: '기타' },
 ];
 
 export default function Profile() {
-  return (
-    <div className='mx-auto flex flex-col items-center justify-center gap-3 p-5'>
-      <p className='text-h3'>회원 정보 수정</p>
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
+  return (
+    <div className='mx-auto flex w-[50%] flex-col items-center justify-center gap-3 p-5'>
+      {/* w-[30%]  */}
+      <p className='text-h3'>회원 정보 수정</p>
       <AvatarProfile />
 
-      <form action='' className='flex flex-col gap-5'>
-        <Input type='email' placeholder='기존 이메일 값' />
+      <form action='' className='flex flex-col gap-5 p-2'>
+        <Input
+          type='email'
+          placeholder='기존 이메일 값'
+          readOnly
+          onFocus={(e) => e.target.blur()}
+        />
         <Input
           type='password'
           placeholder='비밀번호'
-          icon={<Icon name='EYE' size={40} className='bg-black text-black' />}
+          icon={
+            <button
+              type='button'
+              onClick={() => setShowPassword(!showPassword)}
+              className='cursor-pointer'
+            >
+              <Icon
+                name={showPassword ? 'EYE' : 'EYE_SLASHED'}
+                size={20}
+                className='stroke-text-sub'
+              />
+            </button>
+          }
         />
         <Input
           type='password'
           placeholder='비밀번호 확인'
-          icon={<Icon name='EYE' size={40} className='bg-black text-black' />}
+          icon={
+            <button
+              type='button'
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              className='cursor-pointer'
+            >
+              <Icon
+                name={showConfirmPassword ? 'EYE' : 'EYE_SLASHED'}
+                size={20}
+                className='stroke-text-sub'
+              />
+            </button>
+          }
         />
+
         <Input type='text' icon='중복 확인' placeholder='닉네임' />
 
-        <div className=''>
-          <p>관심 장르 설정</p>
+        <div className='pt-2'>
+          <p className='pb-1'>관심 장르 설정</p>
           {genres.map((genre, i) => (
             <GenreButton key={i} genre={genre.name} mode={'toggle'} />
           ))}
         </div>
       </form>
 
-      <div className='flex gap-3'>
+      <div className='flex w-full gap-3'>
         <Button
           variant='secondary'
           children='취소'
