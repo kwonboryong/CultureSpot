d328e06c2b79a93cd19bbe2c74dd05de0681f7a6
kwonboryong <kwonboryong@naver.com>
2025-03-21 21:13:17 +0900

    feat: 모달 오픈을 위한 props 추가 및 DialogTrigger 제거

--- src/components/modal/CommonModal.tsx
+++ src/components/modal/CommonModal.tsx
@@ -3,26 +3,31 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from './CommonDialog';
 
 interface CommonModalProps {
-  variant?: 'delete' | 'fix' | 'withdraw';
+  variant?: 'delete' | 'cancel' | 'withdraw';
   customText?: string;
+  open: boolean;
+  onClose: () => void;
 }
 
 const textVariants = {
   delete: '정말 삭제하시겠습니까?',
-  fix: '수정 사항이 저장되지 않았습니다. \n 계속 하시겠습니까?',
+  cancel: '수정 사항이 저장되지 않았습니다. \n 계속 하시겠습니까?',
   withdraw: '탈퇴 시 모든 정보가 사라집니다. \n 계속 하시겠습니까?',
 };
 
-const CommonModal = ({ variant, customText }: CommonModalProps) => {
+const CommonModal = ({
+  variant,
+  customText,
+  open,
+  onClose,
+}: CommonModalProps) => {
   const modalText = variant ? textVariants[variant] : customText;
 
   return (
-    <Dialog>
-      <DialogTrigger>모달 Open</DialogTrigger>
+    <Dialog open={open} onOpenChange={onClose}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle style={{ whiteSpace: 'pre-line' }}>
