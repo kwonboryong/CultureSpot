1890c88228a3b7cf389565a1fd200e22c3905ec3
HY KIM <chopinoff@gmail.com>
2025-03-22 21:15:30 +0900

    refactor: Header 배경 색상 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -33,7 +33,7 @@ const Header = ({ onToggleSidebar }: HeaderProps) => {
   }, []);
 
   return (
-    <header className='fixed left-0 top-0 flex h-20 w-full items-center justify-between'>
+    <header className='fixed left-0 top-0 flex h-20 w-full items-center justify-between bg-bg'>
       <div className='flex-[3]'>
         <div className='mx-[34px] flex w-[280px] gap-10'>
           <button onClick={onToggleSidebar}>
