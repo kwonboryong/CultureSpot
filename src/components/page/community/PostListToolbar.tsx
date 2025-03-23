33def3d0ee1d9135d8722f0b037a3be918ba324a
HY KIM <chopinoff@gmail.com>
2025-03-24 00:42:25 +0900

    feat: [CUL-120] Post 컴포넌트 개발 (PostListItem, PostListToolbar)

---
+++ src/components/page/community/PostListToolbar.tsx
@@ -0,0 +1,35 @@
+'use client';
+import ListToolbar from './../../common/ListToolbar';
+import Dropdown from './../../common/Dropdown';
+
+interface PostListToolbarProps {
+  postCount: number;
+}
+
+export default function PostListToolbar({ postCount }: PostListToolbarProps) {
+  return (
+    <div className='border-b'>
+      <ListToolbar hideBorder>
+        <div className='flex items-center gap-x-[8px] text-body2 font-semibold text-text-sub'>
+          전체 {postCount.toLocaleString()}개
+        </div>
+        <div>
+          <Dropdown
+            type='select'
+            buttonWidth='sm'
+            buttonHeight='sm'
+            menuWidth='sm'
+            menuHeight='sm'
+            fontSize='sm'
+            menuItems={[
+              { label: '최신순', onClick: () => {} },
+              { label: '댓글많은순', onClick: () => {} },
+              { label: '좋아요순', onClick: () => {} },
+              { label: '과거순', onClick: () => {} },
+            ]}
+          />
+        </div>
+      </ListToolbar>
+    </div>
+  );
+}
