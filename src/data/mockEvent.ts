a57b155d95a5144ae3923916306ad9f6b5089784
HY KIM <chopinoff@gmail.com>
2025-04-04 00:08:44 +0900

    chore: [CUL-133] mockEvents 추가

---
+++ src/data/mockEvent.ts
@@ -0,0 +1,405 @@
+export const mockPerformances = [
+  {
+    id: 1,
+    type: 'performance',
+    title: '뮤지컬 <모리스>',
+    place: '인터파크 서경스퀘어 스콘 1관',
+    startDate: '2025.03.07',
+    endDate: '2025.05.25',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25001387_p.gif',
+  },
+  {
+    id: 2,
+    type: 'performance',
+    title: '뮤지컬 지킬앤하이드 (Jekyll ＆ Hyde) - 20주년',
+    place: '블루스퀘어 신한카드홀',
+    startDate: '2024.11.29',
+    endDate: '2025.05.18',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/24/24013928_p.gif',
+  },
+  {
+    id: 3,
+    type: 'performance',
+    title: '뮤지컬 〈알라딘〉 한국 초연 (ALADDIN The Musical)',
+    place: '샤롯데씨어터',
+    startDate: '2024.11.22',
+    endDate: '2025.06.22',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/24/24012498_p.gif',
+  },
+  {
+    id: 4,
+    type: 'performance',
+    title: '뮤지컬 〈라이카〉',
+    place: '두산아트센터 연강홀',
+    startDate: '2025.03.14',
+    endDate: '2025.05.18',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25001462_p.gif',
+  },
+  {
+    id: 5,
+    type: 'performance',
+    title: '뮤지컬 〈도리안 그레이〉',
+    place: '홍익대 대학로 아트센터 대극장',
+    startDate: '2025.03.30',
+    endDate: '2025.06.08',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25001547_p.gif',
+  },
+  {
+    id: 6,
+    type: 'performance',
+    title: '콜드플레이 내한공연',
+    place: '고양종합운동장 주경기장(자세히)',
+    startDate: '2025.04.16',
+    endDate: '2025.04.25',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/24/24013437_p.gif',
+  },
+  {
+    id: 7,
+    type: 'performance',
+    title: '서울스프링페스타 WONDER SHOW_2차 티켓',
+    place: '서울월드컵경기장',
+    startDate: '2025.04.30',
+    endDate: '2025.04.30',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25002908_p.gif',
+  },
+  {
+    id: 8,
+    type: 'performance',
+    title: '2025 이영현 콘서트 ＂나의 노래가 필요한 너에게＂ - 봄',
+    place: '블루스퀘어 SOL트래블홀',
+    startDate: '2025.05.30',
+    endDate: '2025.06.01',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25004620_p.gif',
+  },
+  {
+    id: 9,
+    type: 'performance',
+    title: '2025 N.Flying LIVE ‘＆CON4 : FULL CIRCLE’',
+    place: '올림픽공원 올림픽홀',
+    startDate: '2025.05.09',
+    endDate: '2025.05.11',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25004056_p.gif',
+  },
+  {
+    id: 10,
+    type: 'performance',
+    title: '2025 ZEROBASEONE FAN-CON［BLUE MANSION］',
+    place: 'KSPO DOME',
+    startDate: '2025.04.18',
+    endDate: '2025.04.20',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25003660_p.gif',
+  },
+  {
+    id: 11,
+    type: 'performance',
+    title: '뮤지컬 <모리스>',
+    place: '인터파크 서경스퀘어 스콘 1관',
+    startDate: '2025.03.07',
+    endDate: '2025.05.25',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25001387_p.gif',
+  },
+  {
+    id: 12,
+    type: 'performance',
+    title: '뮤지컬 지킬앤하이드 (Jekyll ＆ Hyde) - 20주년',
+    place: '블루스퀘어 신한카드홀',
+    startDate: '2024.11.29',
+    endDate: '2025.05.18',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/24/24013928_p.gif',
+  },
+  {
+    id: 13,
+    type: 'performance',
+    title: '뮤지컬 〈알라딘〉 한국 초연 (ALADDIN The Musical)',
+    place: '샤롯데씨어터',
+    startDate: '2024.11.22',
+    endDate: '2025.06.22',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/24/24012498_p.gif',
+  },
+  {
+    id: 14,
+    type: 'performance',
+    title: '뮤지컬 〈라이카〉',
+    place: '두산아트센터 연강홀',
+    startDate: '2025.03.14',
+    endDate: '2025.05.18',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25001462_p.gif',
+  },
+  {
+    id: 15,
+    type: 'performance',
+    title: '뮤지컬 〈도리안 그레이〉',
+    place: '홍익대 대학로 아트센터 대극장',
+    startDate: '2025.03.30',
+    endDate: '2025.06.08',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25001547_p.gif',
+  },
+  {
+    id: 16,
+    type: 'performance',
+    title: '콜드플레이 내한공연',
+    place: '고양종합운동장 주경기장(자세히)',
+    startDate: '2025.04.16',
+    endDate: '2025.04.25',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/24/24013437_p.gif',
+  },
+  {
+    id: 17,
+    type: 'performance',
+    title: '서울스프링페스타 WONDER SHOW_2차 티켓',
+    place: '서울월드컵경기장',
+    startDate: '2025.04.30',
+    endDate: '2025.04.30',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25002908_p.gif',
+  },
+  {
+    id: 18,
+    type: 'performance',
+    title: '2025 이영현 콘서트 ＂나의 노래가 필요한 너에게＂ - 봄',
+    place: '블루스퀘어 SOL트래블홀',
+    startDate: '2025.05.30',
+    endDate: '2025.06.01',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25004620_p.gif',
+  },
+  {
+    id: 19,
+    type: 'performance',
+    title: '2025 N.Flying LIVE ‘＆CON4 : FULL CIRCLE’',
+    place: '올림픽공원 올림픽홀',
+    startDate: '2025.05.09',
+    endDate: '2025.05.11',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25004056_p.gif',
+  },
+  {
+    id: 20,
+    type: 'performance',
+    title: '2025 ZEROBASEONE FAN-CON［BLUE MANSION］',
+    place: 'KSPO DOME',
+    startDate: '2025.04.18',
+    endDate: '2025.04.20',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25003660_p.gif',
+  },
+];
+
+export const mockExhibitions = [
+  {
+    id: 21,
+    type: 'exhibition',
+    title: '크리스챤 디올 : 디자이너 오브 드림스',
+    place: '동대문디자인플라자 아트홀 1관',
+    startDate: '2025.04.19',
+    endDate: '2025.07.13',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25003752_p.gif',
+  },
+  {
+    id: 22,
+    type: 'exhibition',
+    title: '인상파, 모네에서 미국으로: 빛, 바다를 건너다',
+    place: '더현대 서울 ALT. 1',
+    startDate: '2025.02.15',
+    endDate: '2025.05.26',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25001149_p.gif',
+  },
+  {
+    id: 23,
+    type: 'exhibition',
+    title: '［얼리버드］현대카드 컬처프로젝트 29 톰 삭스 전',
+    place: '동대문디자인플라자 뮤지엄',
+    startDate: '2025.04.25',
+    endDate: '2025.07.25',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25002387_p.gif',
+  },
+  {
+    id: 24,
+    type: 'exhibition',
+    title: '불멸의 화가 반 고흐 in 대전',
+    place: '대전시립미술관',
+    startDate: '2025.03.25',
+    endDate: '2025.06.22',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25003731_p.gif',
+  },
+  {
+    id: 25,
+    type: 'exhibition',
+    title: '빛의 거장 카라바조 ＆ 바로크의 얼굴들',
+    place: '예술의전당 한가람미술관 2층',
+    startDate: '2024.11.09',
+    endDate: '2025.04.06',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/24/24016040_p.gif',
+  },
+  {
+    id: 26,
+    type: 'exhibition',
+    title: '［20% 할인＋무하 음료 증정］알폰스 무하 원화전',
+    place: '마이아트뮤지엄',
+    startDate: '2025.03.20',
+    endDate: '2025.07.13',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25001911_p.gif',
+  },
+  {
+    id: 27,
+    type: 'exhibition',
+    title: '［북촌］2025년 3월~4월_어둠속의대화(DIALOGUE IN THE DARK)',
+    place: '북촌 어둠속의대화',
+    startDate: '2010.01.20',
+    endDate: '오픈런',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/24/24017362_p.gif',
+  },
+  {
+    id: 28,
+    type: 'exhibition',
+    title: '［예매할인 20% / 6~7월］ 워너 브롱크호스트',
+    place: '그라운드시소 서촌',
+    startDate: '2025.06.01',
+    endDate: '2025.07.31',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25003628_p.gif',
+  },
+  {
+    id: 29,
+    type: 'exhibition',
+    title: '［최대 30%할인］ 디즈니 100년 특별전',
+    place: 'K현대미술관',
+    startDate: '2024.10.18',
+    endDate: '2025.06.01',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/24/24011855_p.gif',
+  },
+  {
+    id: 30,
+    type: 'exhibition',
+    title: '［얼리버드］ 퓰리처상 사진전 대구',
+    place: '뮤씨엄 대구점',
+    startDate: '2025.04.25',
+    endDate: '2025.06.30',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25003889_p.gif',
+  },
+  {
+    id: 31,
+    type: 'exhibition',
+    title: '크리스챤 디올 : 디자이너 오브 드림스',
+    place: '동대문디자인플라자 아트홀 1관',
+    startDate: '2025.04.19',
+    endDate: '2025.07.13',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25003752_p.gif',
+  },
+  {
+    id: 32,
+    type: 'exhibition',
+    title: '인상파, 모네에서 미국으로: 빛, 바다를 건너다',
+    place: '더현대 서울 ALT. 1',
+    startDate: '2025.02.15',
+    endDate: '2025.05.26',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25001149_p.gif',
+  },
+  {
+    id: 33,
+    type: 'exhibition',
+    title: '［얼리버드］현대카드 컬처프로젝트 29 톰 삭스 전',
+    place: '동대문디자인플라자 뮤지엄',
+    startDate: '2025.04.25',
+    endDate: '2025.07.25',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25002387_p.gif',
+  },
+  {
+    id: 34,
+    type: 'exhibition',
+    title: '불멸의 화가 반 고흐 in 대전',
+    place: '대전시립미술관',
+    startDate: '2025.03.25',
+    endDate: '2025.06.22',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25003731_p.gif',
+  },
+  {
+    id: 35,
+    type: 'exhibition',
+    title: '빛의 거장 카라바조 ＆ 바로크의 얼굴들',
+    place: '예술의전당 한가람미술관 2층',
+    startDate: '2024.11.09',
+    endDate: '2025.04.06',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/24/24016040_p.gif',
+  },
+  {
+    id: 36,
+    type: 'exhibition',
+    title: '［20% 할인＋무하 음료 증정］알폰스 무하 원화전',
+    place: '마이아트뮤지엄',
+    startDate: '2025.03.20',
+    endDate: '2025.07.13',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25001911_p.gif',
+  },
+  {
+    id: 37,
+    type: 'exhibition',
+    title: '［북촌］2025년 3월~4월_어둠속의대화(DIALOGUE IN THE DARK)',
+    place: '북촌 어둠속의대화',
+    startDate: '2010.01.20',
+    endDate: '오픈런',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/24/24017362_p.gif',
+  },
+  {
+    id: 38,
+    type: 'exhibition',
+    title: '［예매할인 20% / 6~7월］ 워너 브롱크호스트',
+    place: '그라운드시소 서촌',
+    startDate: '2025.06.01',
+    endDate: '2025.07.31',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25003628_p.gif',
+  },
+  {
+    id: 39,
+    type: 'exhibition',
+    title: '［최대 30%할인］ 디즈니 100년 특별전',
+    place: 'K현대미술관',
+    startDate: '2024.10.18',
+    endDate: '2025.06.01',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/24/24011855_p.gif',
+  },
+  {
+    id: 40,
+    type: 'exhibition',
+    title: '［얼리버드］ 퓰리처상 사진전 대구',
+    place: '뮤씨엄 대구점',
+    startDate: '2025.04.25',
+    endDate: '2025.06.30',
+    imageUrl:
+      'https://ticketimage.interpark.com/Play/image/large/25/25003889_p.gif',
+  },
+];
