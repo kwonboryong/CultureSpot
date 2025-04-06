9f22501fafc1733c094ddb8566eaa918e889b749
HY KIM <chopinoff@gmail.com>
2025-04-06 17:45:02 +0900

    feat: [CUL-198] 사이드바 오버레이 기능 추가 후 SidebarContent로 분리

--- src/components/common/navbar/Sidebar.tsx
+++ src/components/common/navbar/Sidebar.tsx
@@ -6,6 +6,7 @@ import { HTMLAttributes } from 'react';
 import { cn } from '@/lib/utils';
 import clsx from 'clsx';
 import { SidebarItem } from '@/constants/sidebarItems';
+import SidebarContent from '@/components/common/navbar/SidebarContent';
 import dynamic from 'next/dynamic';
 
 const SidebarProfile = dynamic(() => import('./SidebarProfile'));
@@ -14,77 +15,35 @@ type SidebarProps = {
   currentUrl: string;
   sidebarItems: SidebarItem[];
   initialIndex?: null | number;
-  isCollapsed?: boolean;
+  isSidebarOpen: boolean;
+  onClose: () => void;
 } & HTMLAttributes<HTMLDivElement>;
 
 const Sidebar = ({
   currentUrl,
   sidebarItems,
   initialIndex = null,
-  isCollapsed = false,
+  isSidebarOpen,
+  onClose,
   ...rest
 }: SidebarProps) => {
   return (
-    <aside className='h-lvh w-fit bg-bg' {...rest}>
-      <ul className={cn(isCollapsed ? 'w-[90px]' : 'w-[200px]')}>
-        {currentUrl.startsWith('/my') && (
-          <SidebarProfile isCollapsed={isCollapsed} />
+    <div className='z-[60]' {...rest}>
+      <div
+        className={cn(
+          'fixed inset-0 z-50 bg-bg-dark500 transition-opacity duration-300',
+          isSidebarOpen ? 'opacity-100' : 'pointer-events-none opacity-0'
         )}
-        {sidebarItems.map(({ iconName, label, url }, index) => (
-          <li
-            key={index}
-            className={cn(
-              !isCollapsed &&
-                currentUrl.startsWith(url) &&
-                'bg-primary-main100',
-              !isCollapsed && 'hover:bg-primary-main100',
-              'font-semibold text-text-sub'
-            )}
-          >
-            <Link
-              href={url}
-              className={cn(
-                'gap-x-15 group flex h-[50px] items-center justify-start px-[20px]',
-                isCollapsed ? 'w-[90px]' : 'w-[200px]'
-              )}
-            >
-              <div className='flex w-[50px] flex-col items-center justify-start'>
-                <Icon
-                  name={iconName}
-                  size={18}
-                  className={cn(
-                    'stroke-[2px] group-hover:stroke-primary',
-                    currentUrl.startsWith(url)
-                      ? 'stroke-primary'
-                      : 'stroke-text-sub'
-                  )}
-                />
-                {isCollapsed && (
-                  <p
-                    className={clsx(
-                      'text-caption group-hover:text-primary',
-                      currentUrl.startsWith(url) && 'text-primary'
-                    )}
-                  >
-                    {label}
-                  </p>
-                )}
-              </div>
-              {!isCollapsed && (
-                <p
-                  className={cn(
-                    'group-hover:text-primary',
-                    currentUrl.startsWith(url) && 'text-primary'
-                  )}
-                >
-                  {label}
-                </p>
-              )}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </aside>
+        onClick={onClose}
+      />
+      <SidebarContent
+        currentUrl={currentUrl}
+        sidebarItems={sidebarItems}
+        initialIndex={initialIndex}
+        onClose={onClose}
+        isSidebarOpen={!isSidebarOpen}
+      />
+    </div>
   );
 };
 
