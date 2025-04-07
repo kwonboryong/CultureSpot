3e1e6db990c75cca49409446482385d23036c9df
kwonboryong <kwonboryong@naver.com>
2025-04-07 21:23:08 +0900

    feat: github 및 confluence 아이콘 추가

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,9 @@
+import { FaGithub } from 'react-icons/fa';
+import { SiConfluence } from 'react-icons/si';
 const Footer = () => {
   return (
     <footer className='flex items-center w-full h-48'>
-      <div className='flex flex-row items-center justify-between w-full px-10'>
+      <div className='flex flex-row items-center justify-between w-full px-5'>
         <div>
           <div className='flex items-center gap-2'>
             <img
@@ -14,22 +16,25 @@ const Footer = () => {
           <p className='text-body2 text-text-sub'>
             문화 행사 정보 제공 및 커뮤니티 기반 소통 플랫폼
           </p>
-        </div>
 
-        <div className='flex gap-4'>
-          <a href='https://github.com/culturespot' target='_blank'>
-            GitHub
-          </a>
-          <a
-            href='https://chopinoff.atlassian.net/wiki/spaces/CU/pages'
-            target='_blank'
-          >
-            Confluence
-          </a>
-          <a href='/contact' target='_blank'>
-            Contact
-          </a>
+          <div className='flex gap-4'>
+            <a href='https://github.com/culturespot' target='_blank'>
+              <FaGithub size={25} />
+            </a>
+            <a
+              href='https://chopinoff.atlassian.net/wiki/spaces/CU/pages'
+              target='_blank'
+            >
+              <SiConfluence size={25} />
+            </a>
+          </div>
         </div>
+        <nav className='flex flex-col pr-11'>
+          <a href='/'>Home</a>
+          <a href='/about'>Shows</a>
+          <a href='/faq'>Exhibits</a>
+          <a href='/faq'>Community</a>
+        </nav>
       </div>
 
       {/* <div>
