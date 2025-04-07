b5ea5bf001cc3cd723927670bfb4e2deade12868
kwonboryong <kwonboryong@naver.com>
2025-04-07 21:38:56 +0900

    feat: 로고에 Link 추가 및 저작권 표시 추가

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,45 +1,39 @@
+import Link from 'next/link';
 import { FaGithub } from 'react-icons/fa';
 import { SiConfluence } from 'react-icons/si';
 const Footer = () => {
   return (
-    <footer className='flex items-center w-full h-48'>
+    <footer className='flex items-center w-full h-48 select-none'>
       <div className='flex flex-row items-center justify-between w-full px-5'>
         <div>
-          <div className='flex items-center gap-2'>
+          <Link href='/' className='flex items-center gap-2'>
             <img
               src='/assets/logo-icon.svg'
               className='w-6'
               alt='CultureSpot 로고 아이콘'
             />
             <p className='font-semibold text-body1'>Culture Spot</p>
-          </div>
+          </Link>
+
           <p className='text-body2 text-text-sub'>
             문화 행사 정보 제공 및 커뮤니티 기반 소통 플랫폼
           </p>
-
-          <div className='flex gap-4'>
-            <a href='https://github.com/culturespot' target='_blank'>
-              <FaGithub size={25} />
-            </a>
-            <a
-              href='https://chopinoff.atlassian.net/wiki/spaces/CU/pages'
-              target='_blank'
-            >
-              <SiConfluence size={25} />
-            </a>
-          </div>
+          <p className='pt-2 text-caption text-text-disabled'>
+            © 2025 Culture Spot. All rights reserved.
+          </p>
         </div>
-        <nav className='flex flex-col pr-11'>
-          <a href='/'>Home</a>
-          <a href='/about'>Shows</a>
-          <a href='/faq'>Exhibits</a>
-          <a href='/faq'>Community</a>
-        </nav>
+        <div className='flex gap-4'>
+          <a href='https://github.com/culturespot' target='_blank'>
+            <FaGithub size={25} />
+          </a>
+          <a
+            href='https://chopinoff.atlassian.net/wiki/spaces/CU/pages'
+            target='_blank'
+          >
+            <SiConfluence size={25} />
+          </a>
+        </div>
       </div>
-
-      {/* <div>
-        <p>© 2025 Culture Spot. 사이드 프로젝트 용도입니다.</p>
-      </div> */}
     </footer>
   );
 };
